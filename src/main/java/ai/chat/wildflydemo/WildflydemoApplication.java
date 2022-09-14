1ab326d0f707e977f3e18b25e7821de501d8c510
Jacob Lin <jacoblindev@gmail.com>
2022-09-14 15:56:25 +0800

    init: First commit - demo for deploy to wildfly

---
+++ src/main/java/ai/chat/wildflydemo/WildflydemoApplication.java
@@ -0,0 +1,13 @@
+package ai.chat.wildflydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WildflydemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WildflydemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ai/chat/wildflydemo/WildflydemoApplicationTests.java
@@ -0,0 +1,13 @@
+package ai.chat.wildflydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WildflydemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
